21beedab50089f49fc1559c6e5685e00995ade90
carly <carly@noukod.com>
2018-08-31 19:26:44 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,12 @@
 def using_while
 	levitation_force = 6
-	
+	while 
+		levitation_force < 10
+		puts "hello #{levitation_force}"
+		levitation_force +=1
 	#your code here
 end
+
+puts "hello #{levitation_force}"
 
 
